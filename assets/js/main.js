3f73dfee794c54ec8e44384b67c313ec0294def8
Aldo Rojas Nieves <108553512+Shadow-Miku@users.noreply.github.com>
2024-11-22 17:55:31 -0600

    Responsive Menu Implemetation
    - Added a fully functional responsive navigation menu.
    - Implemented a hamburger menu for smaller resolutions
    - Integrated an overlay with a close button for a seamless user experience.
    - Improved styling for better responsiveness and usability.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }, 1000); // Adjust the timeout as needed
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector("header nav");
+  const closeButton = document.querySelector(".close-menu");
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
+  document.body.appendChild(overlay);
+
+  // Abrir menú
+  burger.addEventListener("click", () => {
+    menu.classList.add("active");
+    overlay.classList.add("show");
+  });
+
+  // Cerrar menú al hacer clic en la "X"
+  closeButton.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.classList.remove("show");
+  });
+
+  // Cerrar menú al hacer clic en un enlace dentro del menú
+  menu.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      menu.classList.remove("active");
+      overlay.classList.remove("show");
+    }
+  });
+});
